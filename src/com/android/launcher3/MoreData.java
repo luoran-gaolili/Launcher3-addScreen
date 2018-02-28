cb48a0a77cc982e18c5828c68f66de6244b8741b
ran.luo <1466746723@qq.com>
2018-02-28 11:19:11 +0800

    Initial commit

---
+++ src/com/android/launcher3/MoreData.java
@@ -0,0 +1,21 @@
+package com.android.launcher3;
+
+/**
+ * Created by zhuzi on 17-5-25.
+ */
+
+public class MoreData {
+    public static final String DOC = "doc";
+    public static final String GALLERY = "gallery";
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
